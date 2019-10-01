09016554397440c622517f1151a24e8c8d86d423
Paul Wu <paulwujr@gmail.com>
2019-09-30 22:09:43 -0400

    added app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var titles = document.getElementsByClassName('title');
+
+Array.from(titles).forEach(function(item){
+   console.log(item); 
+})
+
+
+
+//console.log(Array.isArray(titles));
+//console.log(Array.isArray(Array.from(titles)));
